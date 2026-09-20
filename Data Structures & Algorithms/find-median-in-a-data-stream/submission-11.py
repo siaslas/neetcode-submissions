0835bf005b3596cb6f5f3d45ae9e8ed7cb700248
Tre Huang <148663918+siaslas@users.noreply.github.com>
2026-09-20 14:56:00 -0400

    Add: find-median-in-a-data-stream - submission-11

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-11.py
@@ -0,0 +1,42 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.max_heap = []
+        self.min_heap = []
+        self.even = True
+        self.length = 0 
+
+    def addNum(self, num: int) -> None:
+        if self.length == 0:
+            heapq.heappush(self.max_heap, -num)
+        elif self.even:
+            heapq.heappush(self.min_heap, num)
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+        else:
+            heapq.heappush(self.min_heap, num)
+        
+        if self.length > 2 and -self.max_heap[0] > self.min_heap[0]:
+            left_value = -heapq.heappop(self.max_heap)
+            right_value = heapq.heappop(self.min_heap)
+
+            heapq.heappush(self.max_heap, -right_value)
+            heapq.heappush(self.min_heap, left_value)
+
+        self.even = not self.even
+        self.length += 1
+
+    def findMedian(self) -> float:
+        if not self.max_heap:
+            return float(0)
+        
+        if self.even:
+            left, right = -self.max_heap[0], self.min_heap[0]
+            return (left + right) / 2
+        else:
+            if self.min_heap:
+                return float(min(-self.max_heap[0], self.min_heap[0]))
+            else:
+                return float(-self.max_heap[0])
+            
+        
+        
